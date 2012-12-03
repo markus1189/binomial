4601c777fc9558eec966cbbcc98fad7820a78005
Markus Hauck <markus1189@gmail.com>
2012-12-03 11:07:22 +0100

    Reduce duplication in calculator_spec

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -3,88 +3,45 @@ require 'binomial'
 
 describe "Binomial::Calculator" do
   describe "calculate" do
+    def binom_calc(trials,prob,target)
+      Binomial::Calculator.new(
+        :trials => trials,
+        :probability => prob,
+        :target => target
+      )
+    end
+
     it "should validate probability is > 0" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 0,
-          :target       => 3
-        ).calculate
-      }.to raise_error
+      expect { binom_calc(10,0,3).calculate }.to raise_error
     end
 
     it "should validate probability is < 1" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 1.1,
-          :target       => 3
-        ).calculate
-      }.to raise_error
+      expect { binom_calc(10,1.1,3).calculate }.to raise_error
     end
 
     it "should succeed if probability is >0 and <1" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 0.2,
-          :target       => 3
-        ).calculate
-      }.to_not raise_error
+      expect { binom_calc(10, 0.2, 3).calculate }.to_not raise_error
     end
 
     it "should validate target < trials" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 0.2,
-          :target       => 11
-        ).calculate
-      }.to raise_error
+      expect { binom_calc(10, 0.2, 11).calculate }.to raise_error
     end
 
     it "should succeed if target < trials" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 0.2,
-          :target       => 3
-        ).calculate
-      }.to_not raise_error
+      expect { binom_calc(10, 0.2, 3).calculate }.to_not raise_error
     end
 
     it "should validate target > 0" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 0.2,
-          :target       => 0
-        ).calculate
-      }.to raise_error
+      expect { binom_calc(10, 0.2, 0).calculate }.to raise_error
     end
 
     it "should succeed if target > 0" do
-      expect {
-        Binomial::Calculator.new(
-          :trials       => 10,
-          :probability  => 0.2,
-          :target       => 3
-        ).calculate
-      }.to_not raise_error
+      expect { binom_calc(10, 0.2, 3).calculate }.to_not raise_error
     end
 
     it "should produce the correct results" do
-      Binomial::Calculator.new(
-        :trials       => 2,
-        :probability  => 0.5,
-        :target       => 2
-      ).calculate.should == 0.25
-      
-      Binomial::Calculator.new(
-        :trials       => 2,
-        :probability  => 0.5,
-        :target       => 1
-      ).calculate.should == 0.5
+      binom_calc(2,0.5,2).calculate.should == 0.25
+      binom_calc(2,0.5,1).calculate.should == 0.5
     end
   end
 end
